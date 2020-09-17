d1282313a95e8d63b6cff5518fcc386bb10b13d9
mmullis07 <eddiemull311@gmail.com>
2020-09-17 13:21:54 -0700

    Xs Os without changing

--- src/App.js
+++ src/App.js
@@ -6,15 +6,47 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [...Array(9).fill(" ")],
+      currentPlayer: 1
     }
   }
 
+  handleChange = (i) => {
+    let { squares, currentPlayer } = this.state
+    if(squares[i] !== " " ){
+      console.log(currentPlayer)
+      console.log("bailed early")
+      return 
+    } else if (currentPlayer === 0) {
+      squares[i] = "O"
+      currentPlayer = 1 
+    } else {
+      squares[i] = "X"
+      currentPlayer = 0
+    }
+    console.log(currentPlayer)
+    console.log(squares)
+    this.setState({squares: squares, currentPlayer: currentPlayer})
+  }
+
   render(){
+    let { squares } = this.state
+    let grid = squares.map((square, index) => {
+      return (
+        <Square 
+        handleChange = { this.handleChange }
+        square = { square }
+        index = { index }
+        key = { index }
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id="gameBoard">
+          { grid }
+        </div>
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 class Square extends Component{
   render(){
+    let { index } = this.props
     return(
       <React.Fragment>
-        <div id="square"></div>
+        <div id="square" onClick= { () => this.props.handleChange(index)}>
+          {this.props.square}
+        </div>
       </React.Fragment>
     )
   }
