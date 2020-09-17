b7f0d9c95361cf814a550be1c2f1aedd1bb76166
bnddiep <bach.nhu.diep@gmail.com>
2020-09-17 15:42:16 -0700

    retrieved the array of only Xs

--- src/App.js
+++ src/App.js
@@ -7,29 +7,41 @@ class App extends Component{
     super(props)
     this.state = {
       squares: [...Array(9).fill(" ")],
-      currentPlayer: 1
+      currentPlayer: 1,
     }
   }
-  winConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [0,4,8]
-  ]
+  // possible win condition index combinations
+  
 //get indexes of all X so we have a new array of the index[X] and compare to winCondition
   winCheck = (squares) => {
-    //filter through array and get index
+    let winConditions = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [2,4,6],
+      [0,4,8]
+    ]
+    // map through all squares in the array and get the index if there is an X
     let onlyXs = squares.map((square, index)  => {
       if(square === "X") {
         return index
       } 
+      //filter through array for only numbers (which represent X indexes)
     }).filter(value => typeof value === "number")
-    console.log(onlyXs)
-    return onlyXs
+    console.log("array of Xs: ", onlyXs)
+    // compare onlyXs array to the values of the win conditions
+    // find the index at which the values of winConditions array has the some of the same values of onlyXs
+    console.log(typeof onlyXs)
+    console.log(typeof winConditions[0])
+    console.log("winConditions: ", winConditions[0])
+    // if([0,1,2] == winConditions[0]) {
+    //   return alert("You win!")
+    // }
+    
+
   }
     
     //if the index of Xs matches any winConditions alert "You won"
